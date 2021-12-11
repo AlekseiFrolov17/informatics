f21b01ab391abc2b92c31ea0b13106d278f2fa2a
AlekseiFrolov17 <alex3000and7412@gmail.com>
2021-12-11 19:39:53 +0300

    2 DZ

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-A = float(input())
-B = A + 2
-C = (A + 3) / (A + 2)
-D = (A + 4) / (A + 3)
-E = (A + 5) / (A + 4)
-print("B = ", B, " C = ", C, " D = ", D, " E = ", E)
+A = [1, 2, 3, -2, 4]
+length = len(A)
+for i in range(length):
+    currrent_element = A[i]
+    if currrent_element == 0 or currrent_element -2:
+        continue
+    B = currrent_element + 2
+    C = (currrent_element + 3) / (currrent_element + 2)
+    D = (currrent_element + 4) / (currrent_element + 3)
+    E = (currrent_element + 5) / (currrent_element + 4)
+    print(B, '\n', C, '\n' , D, '\n', E)
