5e051efbe0888678891b62098181fa1ab7e7b8d8
AlekseiFrolov17 <alex3000and7412@gmail.com>
2022-01-26 17:31:18 +0300

    3 DZ

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 A = [1, 2, 3, -2, 4]
 length = len(A)
-for i in range(length):
-    currrent_element = A[i]
-    if currrent_element == 0 or currrent_element -2:
-        continue
-    B = currrent_element + 2
-    C = (currrent_element + 3) / (currrent_element + 2)
-    D = (currrent_element + 4) / (currrent_element + 3)
-    E = (currrent_element + 5) / (currrent_element + 4)
-    print(B, '\n', C, '\n' , D, '\n', E)
+def h(A):
+    global length
+    result_list = []
+    for i in range(length):
+        currrent_element = A[i]
+        if currrent_element == 0 or currrent_element -2:
+            continue
+        B = currrent_element + 2
+        result_list.append(B)
+        C = (currrent_element + 3) / (currrent_element + 2)
+        result_list.append(C)
+        D = (currrent_element + 4) / (currrent_element + 3)
+        result_list.append(D)
+        E = (currrent_element + 5) / (currrent_element + 4)
+        result_list.append(E)
+        print(B, C, D, E)
+    return result_list
+result = h(A)
+result.sort()
+print(result)
